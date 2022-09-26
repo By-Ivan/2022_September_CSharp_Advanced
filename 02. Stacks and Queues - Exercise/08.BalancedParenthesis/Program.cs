4c0e70c90a36f7258e72c68a8a83976cc54ce105
here4daknowledge <mr.ivan.mihaylovv@gmail.com>
2022-09-26 15:23:46 +0300

    900/1200

---
+++ 02. Stacks and Queues - Exercise/08.BalancedParenthesis/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _08.BalancedParenthesis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            bool isBalanced = true;
+            Stack<char> stack = new Stack<char>();
+
+            foreach (char ch in input)
+            {
+                if (ch == '{' || ch == '[' || ch == '(')
+                {
+                    stack.Push(ch);
+                }
+                else if (stack.Count == 0)
+                {
+                    isBalanced = false;
+                    break;
+                }
+                else if (   (stack.Peek() == '{'    && ch == '}')
+                        ||  (stack.Peek() == '['    && ch == ']')
+                        ||  (stack.Peek() == '('    && ch == ')')
+                        )
+                {
+                    stack.Pop();
+                }
+                else
+                {
+                    isBalanced = false;
+                    break;
+                }
+            }
+
+            if (!isBalanced || stack.Count > 0)
+            {
+                Console.WriteLine("NO");
+            }
+            else
+            {
+                Console.WriteLine("YES");
+            }
+        }
+    }
+}
---
+++ 02. Stacks and Queues - Exercise/09.SimpleTextEditor/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _09.SimpleTextEditor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            StringBuilder text = new StringBuilder();
+            Stack<string> operations = new Stack<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] cmd = Console.ReadLine().Split();
+                int commandName = int.Parse(cmd[0]);
+
+                switch (commandName)
+                {
+                    case 1:
+                        text.Append(cmd[1]);
+                        operations.Push(text.ToString());
+                        break;
+
+                    case 2:
+                        int count = int.Parse(cmd[1]);
+                        text.Remove(text.Length - count, count);
+                        operations.Push(text.ToString());
+                        break;
+
+                    case 3:
+                        int index = int.Parse(cmd[1]) - 1;
+                        Console.WriteLine(text.ToString()[index]);
+                        break;
+
+                    case 4:
+                        operations.Pop();
+
+                        if (operations.Count == 0)
+                        {
+                            text = new StringBuilder();
+                        }
+                        else
+                        {
+                            text = new StringBuilder(operations.Peek());
+                        }
+                        break;
+
+                }
+            }
+        }
+    }
+}
