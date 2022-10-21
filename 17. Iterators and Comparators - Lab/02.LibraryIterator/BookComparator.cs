1f754ff0549bd2a174237cd134e42441912c4dcb
here4daknowledge <mr.ivan.mihaylovv@gmail.com>
2022-10-21 09:05:17 +0300

    390/400

---
+++ 17. Iterators and Comparators - Lab/02.LibraryIterator/BookComparator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class BookComparator
+    {
+        public int Compare([AllowNull] Book x, [AllowNull] Book y)
+        {
+            if (x.Title != y.Title)
+            {
+                return x.Title.CompareTo(y.Title);
+            }
+
+            return y.Year.CompareTo(x.Year);
+        }
+    }
+}
