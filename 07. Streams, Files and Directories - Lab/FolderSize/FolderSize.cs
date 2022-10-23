5d90649c13ddb20e375265998a2eac503952408e
here4daknowledge <mr.ivan.mihaylovv@gmail.com>
2022-10-23 18:52:20 +0300

    FolderSize

--- 07. Streams, Files and Directories - Lab/FolderSize/FolderSize.cs
+++ 07. Streams, Files and Directories - Lab/FolderSize/FolderSize.cs
@@ -17,6 +17,37 @@
             DirectoryInfo info = new DirectoryInfo(folderPath);
 
             var directories = info.GetDirectories();
+
+            long totalSize = 0;
+
+            foreach (FileInfo file in info.GetFiles())
+            {
+                totalSize += file.Length;
+            }
+
+            foreach (DirectoryInfo directory in directories)
+            {
+                totalSize += GetSubDirectorySize(directory);
+            }
+
+            File.WriteAllText(outputFilePath, $"{totalSize} KB");
+        }
+
+        public static long GetSubDirectorySize(DirectoryInfo dir)
+        {
+            long totalSize = 0;
+
+            foreach (FileInfo file in dir.GetFiles())
+            {
+                totalSize += file.Length;
+            }
+
+            foreach (DirectoryInfo directory in dir.GetDirectories())
+            {
+                totalSize += GetSubDirectorySize(directory);
+            }
+
+            return totalSize;
         }
     }
 }
